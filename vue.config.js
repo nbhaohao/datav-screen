b897537b917140747782fb6cc2edcb7be0e5dfb8
nbhaohao <nbhaohao@163.com>
2021-03-13 22:19:09 +0800

    config:修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   configureWebpack: {
     resolve: { symlinks: false }
-  }
+  },
+  productionSourceMap: false,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/datav-screen-demo/" : "/"
 };
